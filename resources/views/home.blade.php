427358875e81d1d6c601adcab3e8dababd70006c
lpitto86 <luca.p481@gmail.com>
2024-02-26 19:57:26 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1>
+                Hello World!
+            </h1>
+            <a href="about-page" type="button" class="btn btn-primary">GO TO ABOUT</a>
+        </div>
+    </header>
+</body>
+
+</html>
